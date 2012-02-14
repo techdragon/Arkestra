ae2fc5c2c5a96f020560d69f3069ecb4569a43f9
Bjarni Thorisson <hr.bjarni@gmail.com>
2012-02-14 18:53:09 +0000

    adding plugin for project - not working yet though

---
+++ projects/cms_plugins.py
@@ -0,0 +1,40 @@
+from django import forms
+from django.utils.translation import ugettext as _
+
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+
+from arkestra_utilities.generic_models import ArkestraGenericPlugin
+from arkestra_utilities.generic_models import ArkestraGenericPluginForm
+from arkestra_utilities.mixins import AutocompleteMixin
+
+from contacts_and_people.templatetags.entity_tags import work_out_entity
+
+from models import NewsAndEventsPlugin, NewsArticle, Event
+from mixins import NewsAndEventsPluginMixin
+
+from menu import menu_dict
+
+
+class CMSProjectPlugin(ArkestraGenericPlugin, AutocompleteMixin, CMSPluginBase):
+    model = NewsAndEventsPlugin
+    name = _("News & events")
+    form = NewsAndEventsPluginForm
+    menu_cues = menu_dict
+    fieldsets = (
+        (None, {
+        'fields': (('display', 'layout', 'list_format',),  ( 'format', 'order_by', 'group_dates',), 'limit_to')
+    }),
+        ('Advanced options', {
+        'classes': ('collapse',),
+        'fields': ('entity', 'heading_level', ('news_heading_text', 'events_heading_text'), ('show_previous_events', ),)
+    }),
+    )
+
+    # autocomplete fields
+    related_search_fields = ['entity',]
+    
+    def icon_src(self, instance):
+        return "/static/plugin_icons/projects.png"
+
+plugin_pool.register_plugin(CMSNewsAndEventsPlugin)
--- projects/models.py
+++ projects/models.py
@@ -4,6 +4,8 @@ from datetime import date as pythondate
 from datetime import datetime
 from arkestra_utilities.generic_models import ArkestraGenericModel
 from arkestra_utilities.mixins import URLModelMixin
+from cms.models import CMSPlugin
+from contacts_and_people.models import Entity, Person, Building, default_entity_id
 
 class Project(ArkestraGenericModel, URLModelMixin):
     url_path = "projects"
@@ -30,3 +32,37 @@ class Project(ArkestraGenericModel, URLModelMixin):
             return "Featured project"
         get_when = nice_date(self.date, DATE_FORMAT["date_groups"])
         return get_when
+
+from django.conf import settings
+PLUGIN_HEADING_LEVELS = settings.PLUGIN_HEADING_LEVELS
+PLUGIN_HEADING_LEVEL_DEFAULT = settings.PLUGIN_HEADING_LEVEL_DEFAULT
+class ProjectPlugin(CMSPlugin):
+    heading_text = models.CharField(max_length=25, default=_(u"News"))
+    entity = models.ForeignKey(Entity, null=True, blank=True, 
+        help_text="Leave blank for autoselect", 
+        related_name="%(class)s_plugin")
+    FORMATS = (
+        ("title", u"Title only"),
+        ("details image", u"Details"),
+        )
+    format = models.CharField("Item format", max_length=25,choices = FORMATS, default = "details image")    
+    heading_level = models.PositiveSmallIntegerField(choices = PLUGIN_HEADING_LEVELS, default = PLUGIN_HEADING_LEVEL_DEFAULT)
+    ORDERING = (
+        ("date", u"Date alone"),
+        ("importance/date", u"Importance & date"),
+        )
+    order_by = models.CharField(max_length = 25, choices=ORDERING, default="importance/date")
+    LIST_FORMATS = (
+        ("vertical", u"Vertical"),
+        ("horizontal", u"Horizontal"),
+        )
+    list_format = models.CharField("List format", max_length = 25, choices=LIST_FORMATS, default="vertical")
+    # group_dates = models.BooleanField("Show date groups", default = True)
+    limit_to = models.PositiveSmallIntegerField("Maximum number of items", default = 5, null = True, blank = True, 
+        help_text = u"Leave blank for no limit")
+
+    def sub_heading_level(self): # requires that we change 0 to None in the database
+        if self.heading_level == None: # this means the user has chosen "No heading"
+            return 6 # we need to give sub_heading_level a value
+        else:
+            return self.heading_level + 1 # so if headings are h3, sub-headings are h4
